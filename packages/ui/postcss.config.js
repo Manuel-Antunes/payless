9fc922c2a8f02b080526aabc52836272a309a026
Manuel-Antunes <nerdbroz2@gmail.com>
2022-10-19 19:22:31 -0300

    feat: create a shared ui resources module

---
+++ packages/ui/postcss.config.js
@@ -0,0 +1,7 @@
+// base postcss config
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,13 @@
+// base tailwind config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7159c1',
+      },
+    },
+  },
+  plugins: [],
+};
